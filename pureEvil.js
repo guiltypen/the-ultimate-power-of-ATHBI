3d353efd5f2ce314928c59b1005122e8f4ac5194
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-07 19:17:32 +0300

    kinda works

--- pureEvil.js
+++ pureEvil.js
@@ -80,3 +80,63 @@
 
 ******************Good Luck legends!!!!!******************
  */
+let hight = 10;
+let shape1 = [];
+let shape2 = [];
+let shape3 = [];
+let finalshape = [];
+
+triangle(hight);
+
+//tower.forEach((level) => console.log(level));
+//console.log(shape1);
+
+rectangle(hight);
+
+flip(shape1, hight);
+
+finalshape = shape1.concat(shape2, shape3);
+
+finalshape.forEach((level) => console.log(level));
+
+function triangle(nHight) {
+  let star = "*";
+  let space = " ";
+  let starSpace = "* ";
+  for (let i = 0; i < nHight; i++) {
+    let spacesBefore = space.repeat(nHight - i - 1);
+    spaceInBetween = space.repeat(Math.abs(parseInt(2 * i - 1)));
+
+    sta = spacesBefore + star + spaceInBetween + star + spacesBefore;
+
+    shape1.push(sta);
+  }
+  shape1[0] = space.repeat(nHight - 1) + star + space.repeat(nHight - 1);
+  shape1[nHight - 1] = starSpace.repeat(Math.abs(parseInt(nHight)));
+
+  return shape1;
+}
+function rectangle(nHight) {
+  let star = "*";
+  let space = " ";
+
+  for (let i = 0; i < nHight; i++) {
+    let spacesBefore = space.repeat(nHight / 2);
+
+    sta = spacesBefore + star + spacesBefore + star + spacesBefore;
+
+    shape2.push(sta);
+  }
+  return shape2;
+}
+
+function flip(shape, nHight) {
+  let tempshape = [];
+  for (let i = 1; i < nHight; i++) {
+    tempshape[i] = shape[nHight - i];
+  }
+  tempshape[nHight] = shape[0];
+  shape3 = tempshape.slice(1, nHight + 1);
+
+  return shape3;
+}
